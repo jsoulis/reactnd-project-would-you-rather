ab0d0fa5c0b4a42121c94b6f0826e3a89caa8d03
jsoulis <jtsoulis7@gmail.com>
2020-03-21 16:26:57 +0900

    set up file structure for project, implemented skeleton for reducers and actions and connected App component to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
   componentDidMount() {
-
+    this.props.dispatch(handleInitialData());
   }
 
   render() {
@@ -13,4 +15,10 @@ class App extends Component {
 
 }
 
-export default App;
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App);
